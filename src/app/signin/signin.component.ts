f321f3f3a0997c34e2e902c112441370a24ecb8d
maris-baby-1999 <marisbaby18@gmail.com>
2022-10-16 11:50:46 +0530

    SPA Auth

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.signinform = this.formbuilder.group(
+      {
+        name: [''],
+        email: [''],
+        password: [''],
+      }
+    )
+  }
+
+  signinform!: FormGroup
+  signin() {
+    this.http.get<any>(' http://localhost:3000/user').subscribe(res => {
+      const user = res.find((a: any) => {
+        return a.email === this.signinform.value.email && a.password === this.signinform.value.password
+      });
+      
+      if (user) {
+        alert('user exists!');
+        this.signinform.reset();
+      }else{
+        alert("user not found")
+        this.signinform.reset();
+      }
+    }
+    )
+    
   }
 
 }
